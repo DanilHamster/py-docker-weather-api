f210464840755b645ab9e8e4a7cb08ca2a66bf4a
Hamster <dtaranenko137@gmail.com>
2025-09-08 19:41:42 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import os
 import requests
-from dotenv import load_dotenv
+from dotenv import (
+    load_dotenv,
+)
 
 load_dotenv()
 
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
+
 
 def get_weather() -> None:
-    API_KEY = os.getenv("API_KEY")
-    CITY = "Kiev"
-    URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
-
     response = requests.get(URL)
     data = response.json()
 
-    print(f"Weather in {CITY}: {data['current']['condition']['text']}, {data['current']['temp_c']}°C")
+    print(
+        f"Weather in {CITY}: "
+        f"{data["current"]["condition"]["text"]}, "
+        f"{data["current"]["temp_c"]}°C"
+    )
 
 
 if __name__ == "__main__":
